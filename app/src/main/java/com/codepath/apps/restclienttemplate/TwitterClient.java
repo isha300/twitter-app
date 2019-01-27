06b3d8016dcb01cdac27ad0ac71e5f3e3c294fa2
Isha Kabra <isha.kabra10@gmail.com>
2019-01-27 17:59:39 -0500

    Created timeline activity and custom row layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -24,8 +24,8 @@ import com.loopj.android.http.RequestParams;
 public class TwitterClient extends OAuthBaseClient {
 	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
 	public static final String REST_URL = "https://api.twitter.com/1.1";
-	public static final String REST_CONSUMER_KEY = "4KxocRp2Wh8RZ9cy1KJEjxGVy";
-	public static final String REST_CONSUMER_SECRET = "EeyJ4vEZN3al7c0C13bMwAY3pGc2RASrampYtvJvnX1kLDHKJf";
+	public static final String REST_CONSUMER_KEY = "GWHSAOA0NsikStG0zioRCL9aS";
+	public static final String REST_CONSUMER_SECRET = "LsL0VRVUvOKKb9D9B6BcXtHQbeaP6JREYYPhYxZ3iuBF4aLMtI";
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
